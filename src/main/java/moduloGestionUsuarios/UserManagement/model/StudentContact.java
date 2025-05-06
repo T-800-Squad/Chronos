0b4a08992febbf16bdf65318505634de240d331a
Miguel <miguel.vanegas-c@mail.escuelaing.edu.co>
2025-05-05 19:06:48 -0500

    adding all entities

--- src/main/java/moduloGestionUsuarios/UserManagement/model/StudentContact.java
+++ src/main/java/moduloGestionUsuarios/UserManagement/model/StudentContact.java
@@ -12,4 +12,14 @@ public class StudentContact {
 
     @Id
     private String id_contact;
+
+    public String getCode_student() {
+        return code_student;
+    }
+    public void setCode_student(String code_student) {
+        this.code_student = code_student;
+    }
+    public String getId_contact() {
+        return id_contact;
+    }
 }
