5ab326b14a19befdbc66917b8c05c4549ca74fcb
Miguel <miguel.vanegas-c@mail.escuelaing.edu.co>
2025-05-13 22:28:29 -0500

    cors configuration

--- src/main/java/moduloGestionUsuarios/UserManagement/config/SecurityConfig.java
+++ src/main/java/moduloGestionUsuarios/UserManagement/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("https://9eaizp3wsf.execute-api.us-east-1.amazonaws.com/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
