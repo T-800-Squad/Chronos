ea04a6e8b7e13f603cef319cfdf7dc7b07c2b5aa
Miguel <miguel.vanegas-c@mail.escuelaing.edu.co>
2025-05-20 15:13:18 -0500

    change URL

--- src/main/java/moduloGestionUsuarios/UserManagement/config/SecurityConfig.java
+++ src/main/java/moduloGestionUsuarios/UserManagement/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://jolly-rock-0b67b0810.6.azurestaticapps.net")
+                        .allowedOrigins("https://jolly-rock-0b67b0810.6.azurestaticapps.net, http://localhost:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
